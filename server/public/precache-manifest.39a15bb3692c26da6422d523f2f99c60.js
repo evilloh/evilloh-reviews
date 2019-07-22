b0cf12c03ae4c4f66a653787bb046b35468bc888
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-07-22 02:08:44 +0200

    Deploying

---
+++ server/public/precache-manifest.39a15bb3692c26da6422d523f2f99c60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687be4b34387950cc723af5a11d480ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55f139558e8e792cbcd3",
+    "url": "/static/css/main.483a9fb7.chunk.css"
+  },
+  {
+    "revision": "bcefc538fdeca52e3c22",
+    "url": "/static/js/2.fa087a87.chunk.js"
+  },
+  {
+    "revision": "55f139558e8e792cbcd3",
+    "url": "/static/js/main.2bbcc60f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
